3bf2aec7f8495530690927ee33d11bf9b45e755e
number9141 <number9140@naver.com>
2024-03-15 12:29:15 +0900

    충돌충돌

--- TriPlan/src/main/java/cjm.java
+++ TriPlan/src/main/java/cjm.java
@@ -4,7 +4,7 @@ public class cjm {
 	public static void main(String[] args) {
 
 		System.out.println("쩀, 딸기쨈, 무화과쨈, 사과쨈, ㅁㄴㅇㄹ,ㅁㄴㅇㄹ,ㅁㄴㅇㄹ,ㅁㄴㅇㄹ,ㅁㄴㅇ,ㄻㄴㅇ,ㄻㄴㅇ,ㄻㄴ,ㅇㄻㄴ,ㅇㄹ,");
-
+		System.out.println("qkdhqkdhqkdhqkdhqkdhqkdhkqkdkhqdkdhqkdkhqdkdkqddkhdkqdkhdkdqkhdkqdkhqdkhqdkhqdkqdih");
 	}
 
 }
