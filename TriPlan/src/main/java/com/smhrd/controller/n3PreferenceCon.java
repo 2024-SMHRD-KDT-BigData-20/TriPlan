23d7b556ac62f4e23df4f5343ffbdafa06c33082
smart <smart@smhrd>
2024-03-21 20:41:11 +0900

    asdf

--- TriPlan/src/main/java/com/smhrd/controller/n3PreferenceCon.java
+++ TriPlan/src/main/java/com/smhrd/controller/n3PreferenceCon.java
@@ -17,6 +17,8 @@ public class n3PreferenceCon extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("[n3PreferenceCon]");
 		//인코딩
 		request.setCharacterEncoding("euc-kr");
 		HttpSession session = request.getSession();
